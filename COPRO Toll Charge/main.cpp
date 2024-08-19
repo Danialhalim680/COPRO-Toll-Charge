8bb90e9d0447cb6c8173afcfe7ba77428350c003
Danial Halim <danialhalim680@gmail.com>
2024-08-19 09:18:25 +0800

    Initial Commit

---
+++ COPRO Toll Charge/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  COPRO Toll Charge
+//
+//  Created by Danial Halim on 19/08/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
